7b257280c730b28ad6488562230b7d6c0f8ef2be
Geetha <Geetha@192.168.0.5>
2018-08-15 16:29:47 +0530

    commited latest chnages

---
+++ Test_Jenkins/src/test/java/com/test/Test_Jenkins/NewTest.java
@@ -0,0 +1,23 @@
+package com.test.Test_Jenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  
+//	  System.setProperty("webdriver.gecko.driver", "F:\\software\\geckodriver.exe");
+//	  WebDriver driver = new FirefoxDriver();
+//	  driver.manage().window().maximize();
+//	  driver.get("http://www.facebook.com");
+//	  driver.close();
+	  
+	  System.out.println("----Success---------");
+
+	  System.out.println("----Git testing---------");
+	  
+	  System.out.println("----Jenkins testing---------");
+  }
+}
---
+++ Test_Jenkins/src/test/java/com/test/Test_Jenkins/TestGit.java
@@ -0,0 +1,10 @@
+package com.test.Test_Jenkins;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+
+		System.out.println("Git test");
+	}
+
+}
